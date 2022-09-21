b3a8180315939aea5a7a9e7ddd9c0f9c2d1f8df5
Alperen <dumanalp20@gmail.com>
2022-09-21 20:35:33 +0300

    Initial commit

---
+++ datatypes/datatypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 125;
+        sayi = -129;
+
+        char karakter ='p';
+        String şehir = "Karakter";
+        boolean dogruMu = false;
+        boolean yanlısMI= true;
+
+
+
+
+    }
+}
