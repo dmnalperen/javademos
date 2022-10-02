e3cec71c2df29e0a63a2d2bd0e42163de6926f3f
dmnalperen <113796720+dmnalperen@users.noreply.github.com>
2022-10-02 13:39:42 +0300

    Add files via upload

---
+++ ReCapDemo_Classes/ReCapDemo_Classes/src/DortIslem.java
@@ -0,0 +1,20 @@
+public class DortIslem {
+    public int Topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    public int Çıkarma(int sayi1, int sayi2) {
+        return sayi1 - sayi2;
+
+    }
+
+    public int Çarpma(int sayi1, int sayi2) {
+        return sayi1 * sayi2;
+    }
+
+    public int Bölme(int sayi1, int sayi2) {
+        return sayi1 / sayi2;
+    }
+}
+
+
